ccfffd9a81d2e59d0dff6dc4b1c609f67b76ceba
josearangos <jose.arangos@udea.edu.co>
2018-10-18 23:54:56 -0500

    backend

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 DEBUG = True
-mogobdUrl = ''
 PORT = ''
-APIKEY = ''
-urlPrefix = '/ECS/Api/v1'
+urlPrefix = '/Api/v1'
+MONGO_DBNAME = 'category'
+MONGO_URI = 'mongodb://adminCategory:admin123@ds131973.mlab.com:31973/category-managment'
